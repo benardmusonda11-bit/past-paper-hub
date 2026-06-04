61ec173b8543fda52cc083df5aef8a0c2864aee8
Benard Musonda <benardmusonda@email.com>
2026-06-04 16:36:36 +0200

    fix eslint

--- src/App.js
+++ src/App.js
@@ -162,7 +162,7 @@ export default function PastPaperHub() {
   useEffect(() => {
     if (currentView === 'papers' && selectedGrade && selectedSubject) {
       fetchPapers();
-    }
+    }  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentView, selectedGrade, selectedSubject]);
 
   const fetchPapers = async () => {
